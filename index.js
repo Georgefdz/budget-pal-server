01811f75f856dd414beeeace3430e5179d3eebe8
Georgefdz <georgfdz@gmail.com>
2024-06-05 19:19:18 -0600

    Add: models.js with connection to PostgreSQL database and Expense Table

--- index.js
+++ index.js
@@ -1,43 +1,72 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const sequelize = require('./models');
+
+let myBudget =  [
+  {
+    title: 'Eat Out',
+    amount: 25
+  },
+  {
+    title: 'Rent',
+    amount: 375
+  },
+  {
+    title: 'Grocery',
+    amount: 110
+  }
+];
+
+let friends = [
+  {
+    name: "Carlos",
+    age: 29,
+    location: "Interlomas",
+  },
+  {
+    name: "Iker",
+    age: 29,
+    location: "Interlomas 2",
+  },
+  {
+    name: "Fernando",
+    age: 29,
+    location: "Interlomas 3",
+  }
+];
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.get('/budget', (req, res) => {
-    res.json(
-    //     myBudget: [
-    //         {
-    //             title: 'Eat Out',
-    //             budget: 25
-    //         },
-    //         {
-    //             title: 'Rent',
-    //             budget: 375
-    //         },
-    //         {
-    //             title: 'Grocery',
-    //             budget: 110
-    //         }
-    //     ]
-    // })
-        [{
-            title: 'Eat Out',
-            amount: 25
-        },
-        {
-            title: 'Rent',
-            amount: 375
-        },
-        {
-            title: 'Grocery',
-            amount: 110
-        }]
-)
+    res.json(myBudget);
+    console.log("Concept:", myBudget[0].title);
+    console.log("Amount:", `$${myBudget[0].amount}`);
 });
 
+//Get all friends
+app.get('/friends', (req, res) => {
+    res.status().json(friends);
+    console.log(friends);
+    // console.log(friends[0].name);
+    // console.log(friends[1].name);
+});
+
+//Get a specific friend
+app.get('/friends/:name', (req, res) => {
+  console.log(req.params); //http://localhost:3000/friends/carlos -> will console log: { name: 'carlos' }
+  console.log(req.params.name); //http://localhost:3000/friends/carlos -> will console log: carlos
+  
+  const name = req.params.name.toLowerCase();
+  const filteredFriends = friends.filter((friend) => friend.name.toLowerCase() === name);
+  if (filteredFriends.length === 0) {
+    res.status(404).json({ error: 'Friend not found' });
+  } else {
+    res.status(200).json(filteredFriends);
+  }
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
---
+++ models.js
@@ -0,0 +1,70 @@
+const { Sequelize, DataTypes } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.PG_DATABASE, 
+  process.env.PG_USER, 
+  process.env.PG_PASSWORD, 
+  {
+    host: process.env.PG_HOST,
+    dialect: 'postgres',
+    port: process.env.PG_PORT || 5432,
+    logging: console.log,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      },
+      // connectTimeout: 60000
+    }
+  }
+);
+
+const Expense = sequelize.define('Expense', {
+  concept: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  amount: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  color: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    // defaultValue: "#00587D",
+  },
+  date: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+  isRecurring: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+  }, {
+    timestamps: false,
+  });
+
+  sequelize.sync({ alter: true })
+
+
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+testConnection();
+
+module.exports = {
+  Expense,
+}
