a141386d9c894b72a450090e9dd2c97774e37bde
Georgefdz <georgfdz@gmail.com>
2024-06-05 20:19:54 -0600

    Add: cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 const { Expense } = require('./models');
 
+app.use(cors());
 app.use(express.json());
 
 let myBudget =  [
